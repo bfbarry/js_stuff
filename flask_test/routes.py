1bb5b4f6b0f27ad01d8befc1925fcd0b635084c7
brianatUCSD <45830810+brianatUCSD@users.noreply.github.com>
2021-04-12 13:24:28 -0700

    some reorg

--- flask_test/routes.py
+++ flask_test/routes.py
@@ -1,12 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def template_test():
-    return render_template(
-        'template.html', my_string="Wheeeee!", 
-        my_list=[0,1,2,3,4,5], title="Home")
-
 @app.route("/home")
 def home():
     return render_template(
